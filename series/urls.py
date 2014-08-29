ec0248049c05f018301afd0f185a60f3dfdeae52
kazantsev <eiscalle@gmail.com>
2014-08-29 18:14:06 +0400

    add ajaxuploader

--- series/urls.py
+++ series/urls.py
@@ -1,3 +1,4 @@
+from ajaxuploader.views import AjaxFileUploader
 from django.conf.urls import patterns, url
 from django.contrib.auth.decorators import login_required
 
@@ -9,4 +10,5 @@ urlpatterns = patterns(
     url(r'^(?P<series_id>\d+)/list/$', EpisodeList.as_view(), name='episode_list'),
     url(r'^(?P<series_id>\d+)/detail/(?P<pk>\d+)/$', EpisodeDetail.as_view(), name='episode_detail'),
     url(r'^create/$', login_required(EpisodeCreate.as_view()), name='episode_create'),
+    url(r'^upload/$', login_required(AjaxFileUploader()), name='episode_upload'),
 )
--- settings/settings.py
+++ settings/settings.py
@@ -47,7 +47,8 @@ INSTALLED_APPS = (
     'auth',
     'subtitles',
     'coffin',
-    'storages'
+    'storages',
+    'ajaxuploader'
 )
 
 MIDDLEWARE_CLASSES = (
